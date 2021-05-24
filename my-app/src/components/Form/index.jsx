43605e0f10b62a80f0470c1b40f9cc3049596a6f
Buse Karakas <karakasbuse99@gmail.com>
2021-05-25 02:23:35 +0300

    Updated useEffect and Alerts

--- my-app/src/components/Form/index.jsx
+++ my-app/src/components/Form/index.jsx
@@ -13,8 +13,6 @@ function Form() {
     amount:localStorage.getItem('FormValuesLocalStorage') ? JSON.parse(localStorage.getItem('FormValuesLocalStorage')).amount : '',
   });
 
-
-
   const [idValue, SetIdValue] = useState({
     id:''
   });
@@ -36,7 +34,7 @@ function Form() {
   }
 
   const onlyLettersAndSpace = (p) => {
-    const regex = new RegExp(/^[a-zA-ZğüşöçİĞÜŞÖÇı\s]*$/);   //(/^[a-zA-ZğüşöçİĞÜŞÖÇı\s]*$/)
+    const regex = new RegExp(/^[a-zA-ZğüşöçİĞÜŞÖÇı\s]*$/); 
     return regex.test(p);
   }
 
@@ -138,57 +136,63 @@ function Form() {
     e.preventDefault();
     const {firstName,lastName,email,productName,amount,billNo} = formValues;
    
+    if(!formValues.firstName || !formValues.lastName || !formValues.email|| !formValues.productName || !formValues.billNo || !formValues.amount){
+      alert("Empty fields must be filled out");
 
-    if(!formValues.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-        alert("Invalid E-mail");
     }
     else{
+     if(!formValues.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+        alert("Invalid E-mail");
+      }
+      else{
 
-      console.log(formValues); // Butona bastığımızda bütün formValues değerlerinin gelmesinin kontrolüsss
+        console.log(formValues); // Butona bastığımızda bütün formValues değerlerinin gelmesinin kontrolüsss
 
-      await axios.post('http://localhost:8010/users', {   
-        firstName,                                        // postman'deki body tarafı
-        lastName ,
-        email ,
-      })
-      .then(function (response) {
-        idValue.id=response.data;
-        console.log(idValue.id);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-
-      await axios.post('http://localhost:8010/bills', {
-        productName,
-        amount ,
-        billNo ,
-        userId: idValue.id
-      })
-      .then(function (response) {
-        console.log(response);
-        setFormValues({
-          firstName: '',
-          lastName:'',
-          email: '',
-          billNo:'',
-          productName:'',
-          amount:'',
+        await axios.post('http://localhost:8010/users', {   
+          firstName,                                        // postman'deki body tarafı
+          lastName ,
+          email ,
         })
-        if(!response.status==200)
-        {
-            alert("Failed !!");
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });  
-    }
+        .then(function (response) {
+          idValue.id=response.data;
+          console.log(idValue.id);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
 
-    /*
-    if(formValues.billNo.indexOf(1).match(/^[a-zA-Z]+$/) && formValues.billNo.indexOf(2).match(/^[a-zA-Z]+$/)){
-      if(formValues.billNo.indexOf())
-    }*/
+        await axios.post('http://localhost:8010/bills', {
+          productName,
+          amount ,
+          billNo ,
+          userId: idValue.id
+        })
+        .then(function (response) {
+          console.log(response);
+          setFormValues({
+            firstName: '',
+            lastName:'',
+            email: '',
+            billNo:'',
+            productName:'',
+            amount:'',
+          })
+          if(!response.status==200)
+          {
+              alert("Failed !!");
+          }
+        })
+        .catch(function (error) {
+          console.error(error);
+        });  
+      }
+
+      /*
+      if(formValues.billNo.indexOf(1).match(/^[a-zA-Z]+$/) && formValues.billNo.indexOf(2).match(/^[a-zA-Z]+$/)){
+        if(formValues.billNo.indexOf())
+      }*/
+    }
+    
   }
 
   return (
@@ -197,33 +201,36 @@ function Form() {
     PLEASE FILL THE FORM
   </h3>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle} >FIRST NAME:</label>
+    <label className={styles.lStyle} ><span className={styles.spanStyle}>*</span>FIRST NAME:</label>
     <input className={styles.iStyle} type="text" onChange={(e) => handleFormValuesChange(e,'firstName')} value={formValues.firstName} name="firstName" placeholder="John"></input>
   </div>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle}>LAST NAME:</label>
+    <label className={styles.lStyle}><span className={styles.spanStyle}>*</span>LAST NAME:</label>
     <input className={styles.iStyle} type="text" onChange={(e) => handleFormValuesChange(e,'lastName')} value={formValues.lastName} name="lastName" placeholder="Doe"></input>
   </div>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle}>E-MAIL:</label>
+    <label className={styles.lStyle}><span className={styles.spanStyle}>*</span>E-MAIL:</label>
     <input className={styles.iStyle} type="text"  onChange={(e) => handleFormValuesChange(e,'email')} value={formValues.email} name="email" placeholder="John@Doe.com"></input>
   </div>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle}>PRODUCT NAME:</label>
+    <label className={styles.lStyle}><span className={styles.spanStyle}>*</span>PRODUCT NAME:</label>
     <input className={styles.iStyle} type="text" onChange={(e) => handleFormValuesChange(e,'productName')} value={formValues.productName} name="productName" placeholder="Microphone"></input>
   </div>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle}>AMOUNT:</label>
+    <label className={styles.lStyle}><span className={styles.spanStyle}>*</span>AMOUNT:</label>
     <input className={styles.iStyle} type="text"  onChange={(e) => handleFormValuesChange(e,'amount')} value={formValues.amount} name="amount" placeholder="45"></input>
     </div>
   <div className={styles.formDiv}>
-    <label className={styles.lStyle}>BILL NO:</label>
+    <label className={styles.lStyle}><span className={styles.spanStyle}>*</span>BILL NO:</label>
     <input className={styles.iStyle} type="text" onChange={(e) => handleFormValuesChange(e,'billNo')} value={formValues.billNo} name="billNo" placeholder="TR000"></input>
   </div>
-  <div className={styles.formButton}>
-    <input className={styles.bStyle} type="submit"  value="SEND"></input>
+  <div>
+    <p className={styles.pStyle}>All fields marked with * are required</p>
   </div>
-  
+  <div className={styles.formButton}>
+    
+  <input className={styles.bStyle} type="submit"  value="SEND"></input>
+  </div>
 </form> 
   );
 } 
