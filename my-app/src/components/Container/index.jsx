6917feeabd7b8829655429f8c78e3c582a65bc70
bskrks <47915824+bskrks@users.noreply.github.com>
2021-11-03 16:47:16 +0300

    Update index.jsx

--- my-app/src/components/Container/index.jsx
+++ my-app/src/components/Container/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from'./styles.module.scss';
                      
 
-function Container({children}) {                     // süslü parantez burada; children, props'un bir elemanı anlamı katar
+function Container({children}) {                    
   return (
   <div className={styles.divStyle}>{children}</div> 
   );
